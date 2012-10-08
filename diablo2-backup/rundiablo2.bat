dd9fcf2fd92f81d3d2d8ff114539f810a01e150b
Jason Baker <jason@onejasonforsale.com>
2012-10-08 16:01:53 -0500

    Add diablo 2 backup scripts.

---
+++ diablo2-backup/rundiablo2.bat
@@ -0,0 +1,6 @@
+REM # for instructions, copyright info, and contact info, check out the readme.md file:
+REM # https://github.com/codercowboy/scripts
+
+start cmd.exe /c "C:\scripts\backupdiablo2.bat"
+pause
+start cmd.exe /c "C:\scripts\rundiablo2cmd.bat"
---
+++ diablo2-backup/rundiablo2cmd.bat
@@ -0,0 +1,5 @@
+REM # for instructions, copyright info, and contact info, check out the readme.md file:
+REM # https://github.com/codercowboy/scripts
+
+cd "C:\Program Files\Diablo II\"
+"Diablo II.exe" -direct -txt
