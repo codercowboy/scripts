58c299a256c782410789327db45d97f3b5720166
Jason Baker <jason@onejasonforsale.com>
2020-04-05 20:55:33 -0700

    Update nmd5tool.js

--- src/nmd5tool/nmd5tool.js
+++ src/nmd5tool/nmd5tool.js
@@ -1,3 +1,20 @@
+/*
+	TODO:
+
+	* Add verbose mode 
+	* verifyall should aggregate stats	
+	* add tests, need to test all error case paths ie removed, moved, added
+	* figure out how to webpack a distributable version
+	* add copyright license notice
+	* add usage
+
+	Future Features:
+
+	* Add update checksum mode.
+	* Add joinall etc from old script
+	* Move lib functions to a lib.js
+*/
+
 'use strict';
 
 const fs = require('fs');
@@ -8,6 +25,80 @@ const CHECKSUM_FILE_NAME = "checksum.md5";
 
 var debugMode = false;
 var testMode = false;
+
+const kilobyte = 1024;
+const megabyte = kilobyte * 1024;
+const gigabyte = megabyte * 1024;
+const terabyte = gigabyte * 1024;
+const petabyte = terabyte * 1024;
+
+function getFileSizePretty(byteCount) {
+	var divisor = null;
+	var label = null;
+	if (byteCount >= petabyte) {
+		divisor = petabyte;
+		label = "PB";
+	} else if (byteCount >= terabyte) {
+		divisor = terabyte;
+		label = "TB";
+	} else if (byteCount >= gigabyte) {
+		divisor = gigabyte;
+		label = "GB";
+	} else if (byteCount >= megabyte) {
+		divisor = megabyte;
+		label = "MB";
+	} else if (byteCount >= kilobyte) {
+		divisor = kilobyte;
+		label = "KB";
+	} else {
+		return "" + byteCount.toLocaleString() + " bytes";
+	}
+	byteCount = ((byteCount * 1.0) / (divisor * 1.0));
+	return "" + byteCount.toFixed(2) + label;
+}
+
+const millisInSecond = 1000;
+const millisInMinute = 60 * millisInSecond;
+const millisInHour = 60 * millisInMinute;
+const millisInDay = 24 * millisInHour;
+const millisInWeek = 7 * millisInDay;
+const millisInYear = 365 * millisInDay;
+
+function getTimeHMSPretty(milliseconds) {
+	var result = "";
+	var tmpAmount = 0;
+	if (milliseconds > millisInYear) {
+		tmpAmount = Math.floor(milliseconds / millisInYear);
+		milliseconds -= (tmpAmount * millisInYear);
+		result += tmpAmount + "y";
+	}
+	if (milliseconds > millisInWeek) {
+		tmpAmount = Math.floor(milliseconds / millisInWeek);
+		milliseconds -= (tmpAmount * millisInWeek);
+		result += tmpAmount + "w";
+	}
+	if (milliseconds > millisInDay) {
+		tmpAmount = Math.floor(milliseconds / millisInDay);
+		milliseconds -= (tmpAmount * millisInDay);
+		result += tmpAmount + "d";
+	}
+	if (milliseconds > millisInHour) {
+		tmpAmount = Math.floor(milliseconds / millisInHour);
+		milliseconds -= (tmpAmount * millisInHour);
+		result += tmpAmount + "h";
+	}
+	if (milliseconds > millisInMinute) {
+		tmpAmount = Math.floor(milliseconds / millisInMinute);
+		milliseconds -= (tmpAmount * millisInMinute);
+		result += tmpAmount + "m";
+	}
+	if (milliseconds == 0) {
+		return result + "0s";
+	}
+	tmpAmount = (milliseconds / millisInSecond).toFixed(3);
+	result += tmpAmount + "s";
+	return result;
+}
 
 function log(message, object) {
 	if (object == null) {
@@ -93,7 +184,9 @@ function processArgs() {
 	log("Working Directory: " + cwd);
 }
 
-function md5(directory, file) {
+function md5(filePath) {
+	var directory = path.dirname(filePath);
+	var file = path.basename(filePath);
 	var command = 'cd "' + directory + '" && md5sum -b "' + file + '"';
 	debug("Executing md5sum command: " + command);
 	var md5ExecOutput = "" + exec.execSync(command);
@@ -134,7 +227,9 @@ function mapBy(itemArray, propertyName) {
 
 function getSortedKeys(array) {
 	var keys = Object.keys(array);
-	keys.sort();
+	keys.sort(function (a, b) {
+		return a.toLowerCase().localeCompare(b.toLowerCase());
+	});
 	return keys;
 }
 
@@ -145,6 +240,7 @@ class MD5Result {
 		this.checksum = checksum;
 		this.source = source;
 		this.fileDetails = new Object();
+		this.verifyStatus = new Object();
 		if (isFile(fullFilePath)) {
 			var stats = fs.statSync(fullFilePath);
 			this.fileDetails.basename = path.basename(fullFilePath);
@@ -161,6 +257,8 @@ class MD5ResultDiff {
 		this.oldResults = oldResults;
 		this.newResults = newResults;
 		this.finishedResults = [];
+		this.finishedResultsMap = [];
+		
 		var newMap = mapBy(newResults, "fileRelativePath");
 		var missingOldResults = [];		
 		var addTime = new Date().toDateString();
@@ -172,16 +270,11 @@ class MD5ResultDiff {
 			}			
 			delete newMap[oldResult.fileRelativePath];
 			if (oldResult.checksum == newResult.checksum) {
-				oldResult.verifyStatus = "VERIFIED";
-				this.incrementCount("Verified");
-				this.finishedResults.push(oldResult);
+				this.addResult(oldResult, false, "Verified", null);
 				continue;
 			} else {
 				debugobj(oldResult, newResult);
-				oldResult.verifyStatus = "FAILED (" + oldResult.checksum + " -> " + newResult.checksum + ")";
-				this.incrementCount("Failed");
-				this.incrementCount("Total Errors");
-				this.finishedResults.push(oldResult);
+				this.addResult(oldResult, true, "Failed", " (" + oldResult.checksum + " -> " + newResult.checksum + ")");
 				continue;
 			}
 		}
@@ -189,49 +282,78 @@ class MD5ResultDiff {
 		for (var oldResult of missingOldResults) {
 			var newResult = leftoverNewFilesChecksumMap[oldResult.checksum];
 			if (newResult == null) {
-				oldResult.verifyStatus = "REMOVED";
-				this.incrementCount("Removed");
-				this.incrementCount("Total Errors");
-				this.finishedResults.push(oldResult);
+				this.addResult(oldResult, true, "Removed", null);
 				continue;
 			}
 			delete leftoverNewFilesChecksumMap[oldResult.checksum];
-			oldResult.verifyStatus = "MOVED -> " + newResult.fileRelativePath;
-			this.incrementCount("Moved");
-			this.incrementCount("Total Errors");
-			finishedResults.push(oldResult);
+			this.addResult(oldResult, true, "Moved", " -> " + newResult.fileRelativePath);
 		}
 		for (var newResult of leftoverNewFilesChecksumMap) {
 			if (newResult == null) {
 				continue;
 			}
-			newResult.verifyStatus = "ADDED " + addTime;
-			this.incrementCount("Added");
-			this.incrementCount("Total Errors");
-			this.finishedResults.push(newResult);
+			this.addResult(newResult, true, "Added", "Added " + addTime);
 		}
 
-		for (var key of getSortedKeys(this.finishedResults)) {
-			var result = this.finishedResults[key];
-			log(result.fileRelativePath + " " + result.verifyStatus);
+		this.printStatus();
+	}
+
+	printStatus() {
+		for (var key of getSortedKeys(this.finishedResultsMap)) {
+			var results = this.finishedResultsMap[key]; 
+			log(key + " (" + results.length + " files):");
+			var resultsMap = mapBy(results, "fileRelativePath");
+			for (var resultKey of getSortedKeys(resultsMap)) {
+				var result = resultsMap[resultKey];
+				log(result.fileRelativePath + " " + result.verifyStatus.description);
+			}	
+			log("");		
 		}
 
 		log("\nVerification Results");
 		for (var countKey of getSortedKeys(this.counts)) {
 			log("  " + countKey + ": " + this.counts[countKey]);
 		}
-		log("\n  Final Status: " + (this.errorsOccurred ? "ERROR" : "SUCCESS"));
+		log("  Final Status: " + (this.errorsOccurred ? "ERROR" : "SUCCESS") + "\n\n");
+	}
+
+	addResult(md5Result, isError, status, statusDesc) {
+		statusDesc = status.toUpperCase() + ((statusDesc == null) ? "" :  " " + statusDesc);
+		md5Result.verifyStatus.description = statusDesc;
+		md5Result.verifyStatus.isError = isError;
+		md5Result.verifyStatus.status = status.toUpperCase();
+		this.incrementCount("Total Files");
+		this.incrementCount(status);
+		if (isError) {
+			this.incrementCount("Total Errors");
+			this.errorsOccurred = true;
+		}
+		this.finishedResults.push(md5Result);
+		var key = status.toUpperCase();
+		if (this.finishedResultsMap[key] == null) {
+			this.finishedResultsMap[key] = [];
+		}
+		this.finishedResultsMap[key].push(md5Result);
 	}
 	
 	incrementCount(countName) {
-		if ("Total Errors" == countName) {
-			this.errorsOccurred = true;
-		}
 		if (this.counts[countName] == null) {
 			this.counts[countName] = 0;
 		}
 		this.counts[countName] += 1;
 	}
+
+	getFilesWithStatus(status) {
+		var results = [];
+		for (var result of this.finishedResults) {
+			if (status.verifyStatus.toLowerCase().indexOf(status) != -1) {
+				results.push(result);
+			}
+		}
+		return results;
+	}
+
+	getVerifiedFiles() { return this.getFilesWithStatus("VERIFIED"); }
 }
 
 function getChecksums(directory) {
@@ -241,17 +363,16 @@ function getChecksums(directory) {
 		if (isDir(fullFilePath)) {
 			debug("+++ Skipping directory: " + file);
 			continue;
-		} else if (isHiddenFile(fullFilePath)) {
-			debug("+++ Skipping hidden file: " + file);
-			continue;
 		} else if (fullFilePath.indexOf(CHECKSUM_FILE_NAME) != -1) {
 			debug("+++ Skipping checksum file: " + file);
 			continue;
 		}
 		debug("+++ Processing File: " + file);
-		var checksum = md5(directory, file);
+		var checksum = md5(fullFilePath);
 
-		result.push(new MD5Result(file, fullFilePath, checksum, "file system"));
+		var md5Result = new MD5Result(file, fullFilePath, checksum, "file system");
+		result.push(md5Result);
+		debug("Parsed md5 result.", md5Result);
 		debug("+++ Finished processing file: " + file);
 		debug("");
 	}
@@ -270,7 +391,7 @@ function readChecksumsFromFile(directory) {
 		lineNumber += 1;
 		debug("Processing line #" + lineNumber + ": " + line);
 		if (line.indexOf("# File: ") == 0) {
-			//example line: # File: Eloquent_JavaScript.mobi :: {"basename":"Eloquent_JavaScript.mobi","size":2265889,"modTimeMs":1585984751704.6675}
+			//example line: # File: file.mobi :: Added Sat Apr 04 2020 # {"basename":"file.mobi","size":2265889,"modTimeMs":1585984751704}
 			debug("Found status line: " + line);
 			lastStatusLine = line;
 			continue;
@@ -279,27 +400,25 @@ function readChecksumsFromFile(directory) {
 			debug("Skipping line: '" + line + "'");
 			continue;
 		}
-		// example line: 7f2501e1d8c37ad446be0fe0d612d240  blah/Eloquent_JavaScript.mobi # Added Sat Apr 04 2020
+		// example line: 7f2501e1d8c37ad446be0fe0d612d240 *./file.mobi
 		var spaceLocation = line.indexOf(" ");
-		var commentLocation = line.indexOf("#");
 		if (spaceLocation == -1) {
 			error("Cannot parse line #" + lineNumber + ": " + line);
 			lastStatusLine = null;
 			continue;
 		}
 		var checksum = line.substr(0, spaceLocation);
-		var fileRelativePath = line.substr(spaceLocation + 2);
-		if (commentLocation != -1) {
-			var length = commentLocation - spaceLocation - 3;
-			console.log(length);
-			var fileRelativePath = line.substr(spaceLocation + 2, length);
-		}		
-		var status = commentLocation == -1 ? null : line.substr(commentLocation + 2);
+		var fileRelativePath = line.substr(spaceLocation + 4);
+		var fullFilePath = path.dirname(checksumFile) + path.sep + fileRelativePath;
+		var md5Result = new MD5Result(fileRelativePath, fullFilePath, checksum, checksumFile);
 
-		var md5Result = new MD5Result(fileRelativePath, directory + path.sep + fileRelativePath, checksum, checksumFile);
-		md5Result.status = status;
 		if (lastStatusLine != null) {
-			md5Result.fileDetails = JSON.parse(lastStatusLine.substr(lastStatusLine.indexOf("::") + 3));
+			var statusLocation = lastStatusLine.indexOf("::");
+			var commentLocation = lastStatusLine.substr(2).indexOf("#");
+			md5Result.status = lastStatusLine.substr(statusLocation + 3, commentLocation - statusLocation - 2);
+			var json = lastStatusLine.substr(commentLocation + 3);
+			debugobj(json);
+			md5Result.fileDetails = JSON.parse(json);
 		}
 		lastStatusLine = null;
 		debug("Parsed md5 result.", md5Result);
@@ -310,36 +429,54 @@ function readChecksumsFromFile(directory) {
 }
 
 function createChecksumFile(directory) {
-	var fileContents = "";
+	var startTime = Date.now()
+	var checksumFile = directory + path.sep + CHECKSUM_FILE_NAME;
+	process.stdout.write("Creating: " + checksumFile + " ");
 	var md5Results = getChecksums(directory);
 	var addTime = new Date().toDateString();
+	var totalSize = 0;
+	var fileContents = "";
 	for (var result of md5Results) {
 		var status = "";
 		if (result.fileDetails != null) {
-			status += "# File: " + result.fileDetails.basename + " :: " + JSON.stringify(result.fileDetails) + "\n";
+			status += "# File: " + result.fileDetails.basename 
+			if (result.status == null) {
+				result.status = "Added " + addTime;
+			}
+			status += " :: " + result.status;
+			status += " # " + JSON.stringify(result.fileDetails) + "\n";
+			totalSize += result.fileDetails.size;
 		}
-		status += result.checksum + "  " + result.fileRelativePath;
-		if (result.status == null) {
-			result.status = "Added " + addTime;
-		}
-		status += " # " + result.status;
+		status += result.checksum + " *./" + result.fileRelativePath;
+		
 		fileContents += status + "\n";
 	}
-	var checksumFile = directory + path.sep + CHECKSUM_FILE_NAME;
-	log("Creating: " + checksumFile);
+	var timeElapsed = getTimeHMSPretty(Date.now() - startTime);
+	var message = "[" + md5Results.length + " files, " + getFileSizePretty(totalSize) + ", " + timeElapsed + "]";
+	fileContents += "# Created " + addTime + " " + message;
+	process.stdout.write(message + "\n");
 	if (debugMode) {
 		debug("Checksum File Contents: \n" + fileContents + "\n\n");
 	}
 	fs.writeFileSync(checksumFile, fileContents);	
+	return { totalSize:totalSize, fileCount:md5Results.length };
 }
 
 function createChecksumForEach(directory) {
+	var totalSize = 0;
+	var fileCount = 0;
+	var checksumFileCount = 0;
 	for (var file of fs.readdirSync(directory)) {
 		var filePath = directory + path.sep + file;
-		if (isDir(file)) {
-			createChecksumFile(filePath);
+		if (isDir(filePath)) {
+			var result = createChecksumFile(filePath);
+			totalSize += result.totalSize;
+			fileCount += result.fileCount;
+			checksumFileCount += 1;
 		}
 	}
+	console.log("Created " + checksumFileCount + " " + CHECKSUM_FILE_NAME + " files. Indexed " 
+		+ fileCount.toLocaleString() + " files, " + getFileSizePretty(totalSize) + ".");
 }
 
 function verifyChecksums(directory) {
@@ -356,7 +493,18 @@ function verifyChecksums(directory) {
 	var diff = new MD5ResultDiff(oldResults, currentResults);
 }
 
+function verifyAllChecksums(directory) {
+	for (var file of listFiles(directory)) {
+		if (file.indexOf(CHECKSUM_FILE_NAME) != -1) {
+			var fullFilePath = directory + path.sep + file;
+			var checksumDirectory = path.dirname(fullFilePath);
+			verifyChecksums(checksumDirectory);
+		}
+	}
+}
+
 async function main() {
+	var startTime = Date.now();
 	processArgs();
 	if (mode == "create") {
 		createChecksumFile(cwd);
@@ -364,7 +512,11 @@ async function main() {
 		createChecksumForEach(cwd);
 	} else if (mode == "verify") {
 		verifyChecksums(cwd);
+	} else if (mode == "verifyall") {
+		verifyAllChecksums(cwd);
 	}	
+	var timeElapsed = Date.now() - startTime;
+	log("Execution Time: " + getTimeHMSPretty(timeElapsed));
 	
 	exit(0);
 }
