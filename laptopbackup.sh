655a3710ee40d920737d99cf3810980d88a832e4
Jason Baker <jason@onejasonforsale.com>
2026-04-16 15:05:19 -0700

    backup script updates

--- laptopbackup.sh
+++ laptopbackup.sh
@@ -199,13 +199,13 @@ if [ "${FLAG_BACKUP_CLEAN}" = "true" ]; then
 	echo "[Starting Clean Backup Step.]"
 
 	echo "+++ Removing 'target' directories from code"
-	for FILE in `find ${CODE} -type d -name target`; do
+	for FILE in `find "${CODE}" -type d -name target`; do
 		echo "Removing: ${FILE}"
 		rm -Rf "${FILE}"		
 	done
 
 	echo "+++ Removing 'node_modules' directories from code"
-	for FILE in `find ${CODE} -type d -name node_modules -d 3`; do
+	for FILE in `find "${CODE}" -type d -name node_modules -d 3`; do
 		echo "Removing: ${FILE}"
 		rm -Rf "${FILE}"
 	done
@@ -227,29 +227,32 @@ if [ "${FLAG_BACKUP_LOCAL}" = "true" ]; then
 	echo "backing up misc"
 	mkdir -p "${LOCAL_RSYNC_TARGET_DIR}/System/"
 	MISC_FOLDER="${LOCAL_RSYNC_TARGET_DIR}/System/misc"
-	rm -Rvf ${MISC_FOLDER}
-	mkdir -p ${MISC_FOLDER}
+	rm -Rvf "${MISC_FOLDER}"
+	mkdir -p "${MISC_FOLDER}"
 
-	cp /etc/hosts ${MISC_FOLDER}/
-	cp /etc/profile ${MISC_FOLDER}/
-	cp /etc/paths ${MISC_FOLDER}/
-	cp ${MY_USER_HOME}/.bash_profile ${MISC_FOLDER}/
-	cp ${MY_USER_HOME}/.bash_logout ${MISC_FOLDER}/
-	cp ${MY_USER_HOME}/setupenv.sh ${MISC_FOLDER}/
-	cp ${MY_USER_HOME}/.gitconfig ${MISC_FOLDER}/
-	cp -r ${CODE}/tools/git ${MISC_FOLDER}/
-	cp -r ${MY_USER_HOME}/.ssh ${MISC_FOLDER}/
-	cp -r ${MY_USER_HOME}/.vnc ${MISC_FOLDER}/
-	cp -r ${MY_USER_HOME}/.subversion ${MISC_FOLDER}/
-	cp -r ${MY_USER_HOME}/.config/filezilla ${MISC_FOLDER}/	
-	cp ${MY_USER_HOME}/.m2/settings.xml ${MISC_FOLDER}/maven.settings.xml
+	cp "/etc/hosts" "${MISC_FOLDER}/"
+	cp "/etc/profile" "${MISC_FOLDER}/"
+	cp "/etc/paths" "${MISC_FOLDER}/"
+	cp "${MY_USER_HOME}/.bash_profile" "${MISC_FOLDER}/"
+	cp "${MY_USER_HOME}/.bash_logout" "${MISC_FOLDER}/"
+	cp "${MY_USER_HOME}/setupenv.sh" "${MISC_FOLDER}/"
+	cp "${MY_USER_HOME}/.gitconfig" "${MISC_FOLDER}/"
+	cp -r "${CODE}/tools/git" "${MISC_FOLDER}/"
+	rm -Rf "${MISC_FOLDER}/.ssh"
+	md5tool.sh CREATE "${MY_USER_HOME}/.ssh"
+	cp -r "${MY_USER_HOME}/.ssh" "${MISC_FOLDER}/"
+	cp -r "${MY_USER_HOME}/.vnc" "${MISC_FOLDER}/"
+	cp -r "${MY_USER_HOME}/.subversion" "${MISC_FOLDER}/"
+	cp -r "${MY_USER_HOME}/.config/filezilla" "${MISC_FOLDER}/"	
+	cp "${MY_USER_HOME}/.m2/settings.xml" "${MISC_FOLDER}/maven.settings.xml"
+	cp "${MY_USER_HOME}/Library/Preferences"/DOSBox*Preferences "${MISC_FOLDER}/"	
 	
-	mkdir -p ${MISC_FOLDER}/sublimetext
+	mkdir -p "${MISC_FOLDER}/sublimetext"
 	cp -r "${MY_USER_HOME}/Library/Application Support/Sublime Text 3/Packages/User" "${MISC_FOLDER}/sublimetext"
-	rm ${MISC_FOLDER}/automator_services.zip
-	zip -r ${MISC_FOLDER}/automator_services.zip "${MY_USER_HOME}/Library/Services/"
-	brew leaves > ${MISC_FOLDER}/brewlist.txt
-	defaults read /Library/Preferences/com.apple.TimeMachine SkipPaths > ${MISC_FOLDER}/timemachine_excludes.txt
+	rm "${MISC_FOLDER}/automator_services.zip"
+	zip -r "${MISC_FOLDER}/automator_services.zip" "${MY_USER_HOME}/Library/Services/"
+	brew leaves > "${MISC_FOLDER}/brewlist.txt"
+	defaults read /Library/Preferences/com.apple.TimeMachine SkipPaths > "${MISC_FOLDER}/timemachine_excludes.txt"
 	find /Applications -maxdepth 1 | sort > "${MISC_FOLDER}/apps.txt"
 	date > "${LOCAL_RSYNC_TARGET_DIR}/backupdate.txt"
 
@@ -273,11 +276,17 @@ if [ "${FLAG_BACKUP_LOCAL}" = "true" ]; then
 
 	echo "backing up games (minecraft, terraria, factorio)"
 	mkdir -p "${LOCAL_RSYNC_TARGET_DIR}/Games/"
-	mkdir -p "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft"
-	my_rsync "${MY_USER_HOME}/Library/Application Support/minecraft/screenshots/" "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft/screenshots/"
-	my_rsync "${MY_USER_HOME}/Library/Application Support/minecraft/saves/" "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft/saves/"	
-	my_rsync "${MY_USER_HOME}/Library/Application Support/Terraria" "${LOCAL_RSYNC_TARGET_DIR}/Games/"
-	my_rsync "${MY_USER_HOME}/Library/Application Support/factorio" "${LOCAL_RSYNC_TARGET_DIR}/Games/"
+	if [ -e "${MY_USER_HOME}/Library/Application Support/minecraft/" ]; then
+		mkdir -p "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft"
+		my_rsync "${MY_USER_HOME}/Library/Application Support/minecraft/screenshots/" "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft/screenshots/"
+		my_rsync "${MY_USER_HOME}/Library/Application Support/minecraft/saves/" "${LOCAL_RSYNC_TARGET_DIR}/Games/Minecraft/saves/"	
+	fi
+	if [ -e "${MY_USER_HOME}/Library/Application Support/Terraria" ]; then
+		my_rsync "${MY_USER_HOME}/Library/Application Support/Terraria" "${LOCAL_RSYNC_TARGET_DIR}/Games/"
+	fi
+	if [ -e "${MY_USER_HOME}/Library/Application Support/factorio"]; then
+		my_rsync "${MY_USER_HOME}/Library/Application Support/factorio" "${LOCAL_RSYNC_TARGET_DIR}/Games/"
+	fi
 
 	echo "Creating md5 in ${LOCAL_RSYNC_TARGET_DIR}"
 	md5tool.sh CREATE "${LOCAL_RSYNC_TARGET_DIR}"
